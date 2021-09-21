705345a33e9c16db1d1a09403112e914c49b2d07
Raymond Padilla <raypadilla615@gmail.com>
2021-09-21 14:50:04 -0400

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 //72c5d232e8555335b975c55d55dbd3e5
 
-const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=72c5d232e8555335b975c55d55dbd3e5&page=1';
-const IMG_API = 'https://image.tmdb.org/t/p/w1280/';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=72c5d232e8555335b975c55d55dbd3e5&query=';
-
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=72c5d232e8555335b975c55d55dbd3e5&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280/";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=72c5d232e8555335b975c55d55dbd3e5&query=";
 
 function App() {
-  const [ movies, setMovies ] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getMovies(FEATURED_API);
-  }, [])
+  }, []);
 
   const getMovies = (API) => {
     fetch(API)
-    .then(res => res.json())
-    .then(data => {
-      setMovies(data.results);
-    });
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    if(searchTerm) {
-      fetch(SEARCH_API+searchTerm)
-          .then(res => res.json())
-          .then(data => {
-              setMovies(data.results);
-          });
+    if (searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+        });
 
-          setSearchTerm('');
+      setSearchTerm("");
     }
   };
 
@@ -42,23 +43,23 @@ function App() {
     setSearchTerm(e.target.value);
   };
 
-  return (<>
-    <header>
-      <form onSubmit={handleOnSubmit}>
-        <input 
-          className="search" 
-          type="search" 
-          placeholder="Search a Movie..." 
-          value={searchTerm}
-          onChange={handleOnChange}
-          /> 
-      </form>
-    </header>
-    <div className="movie-container">
-        {movies.length > 0 && movies.map((movie) => 
-        <Movie key=
-        {movie.id} {...movie} />)}
-    </div>
+  return (
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search a Movie..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </>
   );
 }
